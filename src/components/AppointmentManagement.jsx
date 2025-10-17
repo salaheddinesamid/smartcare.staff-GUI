fd36073631a2a4217424eb68f687d53bde88d351
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-10-17 11:14:35 +0100

    update feature

--- src/components/AppointmentManagement.jsx
+++ src/components/AppointmentManagement.jsx
@@ -1,18 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Appointment.css";
 import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material"
+import axios from "axios";
 
 export const AppointmentManagement = ()=>{
 
+    const APPOINTMENT_SERVICE_URI = process.env.REACT_APP_APPOINTMENT_SERVICE;
+
     const filters = [
-        {id : 1, name: "Recent", value : "New"},
-        {id : 1, name: "Completed", value : "Completed"}
+        {id : 1, name: "Recent", value : "SCHEDULED"},
+        {id : 1, name: "Completed", value : "COMPLETED"}
     ]
+    const [appointments,setAppointments] = useState([]);
     const [currentFilter,setCurrentFilter] = useState("All");
+    const [loading,setLoading] = useState(false);
 
     const handleChangeFilter = (filter) =>{
         setCurrentFilter(filter);
     }
+
+    //const filteredAppointments = appointments.filter((appointment)=> appointment.status === currentFilter);
+
+    const fetchAppointments = async()=>{
+        let uri = APPOINTMENT_SERVICE_URI + "/api/appointment/get_all";
+        try{
+            setLoading(true);
+            let res = await axios.get(uri);
+            setAppointments(res.data.data);
+            console.log(res)
+
+        }
+        catch(err){
+            console.log(err);
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+
+    useEffect(()=>{
+        fetchAppointments();
+    },[])
 
 
 
@@ -48,7 +76,16 @@ export const AppointmentManagement = ()=>{
                         </TableHead>
 
                         <TableBody>
-
+                            {appointments && appointments.map((appointment)=>(
+                                <TableRow>
+                                    <TableCell>{appointment.startDate}</TableCell>
+                                    <TableCell>{appointment.doctorInformation.firstName}</TableCell>
+                                    <TableCell></TableCell>
+                                    <TableCell></TableCell>
+                                    <TableCell></TableCell>
+                                    <TableCell></TableCell>
+                                </TableRow>
+                            ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
